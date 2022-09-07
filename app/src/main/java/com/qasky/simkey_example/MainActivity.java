92ddfb91f1e3aee566a4699a7c48343c923472ed
qinzhongbin <qinzhongbin.top@gmail.com>
2022-09-07 10:50:01 +0800

    添加独立存储设备文件功能

--- app/src/main/java/com/qasky/simkey_example/MainActivity.java
+++ app/src/main/java/com/qasky/simkey_example/MainActivity.java
@@ -117,8 +117,8 @@ public class MainActivity extends AppCompatActivity {
 
         qCard.setLogCallBack();
 
-//        boolean success = qCard.setRedirectFilePath(getFilesDir().getAbsolutePath());
-//        ToastUtils.showLong("设置设备文件路径" + (success ? "成功" : "失败"));
+        boolean success = qCard.setRedirectFilePath(getFilesDir().getAbsolutePath());
+        ToastUtils.showLong("设置设备文件路径" + (success ? "成功" : "失败"));
     }
 
     private void resetParams() {
@@ -339,94 +339,95 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void negoOLBizKey(View view) {
-//        new Thread(() -> {
-//            runOnUiThread(waitingDialog::show);
-//            // step 1: 服务端创建在线业务密钥
-//            Calendar calendar = Calendar.getInstance();
-//            calendar.add(Calendar.YEAR, 1);
-//            CreateOLBizKeyReq createRequest = new CreateOLBizKeyReq();
-//            createRequest.setSecretSize(softKeyLen);
-//            createRequest.setValidityDate(TimeUtils.date2String(calendar.getTime()));
-//            createRequest.setSystemId(systemId);
-//            createRequest.setServerId(keyAppSvrId);
-//            createRequest.setTimestamp(System.currentTimeMillis());
-//            String authMsg_create = createRequest.getSecretSize() + "," + createRequest.getValidityDate() + "," + createRequest.getSystemId() + "," + createRequest.getServerId() + "," + createRequest.getTimestamp();
-//            createRequest.setHmac(Base64.toBase64String(SM3Util.hmac(hmacKey, authMsg_create.getBytes(StandardCharsets.UTF_8))));
-//            try {
-//                Response response_create = okHttpClient.newCall(new Request.Builder()
-//                        .url("https://" + host + "/onlinebizkey/createOnlineBizKey")
-//                        .post(RequestBody.create(GsonUtils.toJson(createRequest), MediaType.parse("application/json; charset=utf-8")))
-//                        .build()).execute();
-//                if (response_create.isSuccessful()) {
-//                    CreateOLBizKeyResp createResponse = GsonUtils.fromJson(response_create.body().string(), CreateOLBizKeyResp.class);
-//                    if (createResponse.getCode() == 0) {
-//                        String secretId = createResponse.getData().getSecretId();
-//
-//                        // step 2: 服务端协商在线业务密钥
-//                        SvrNegoOLBizKeyReq svrNegoReq = new SvrNegoOLBizKeyReq();
-//                        svrNegoReq.setSecretId(secretId);
-//                        svrNegoReq.setSystemId(systemId);
-//                        svrNegoReq.setServerId(keyAppSvrId);
-//                        svrNegoReq.setTimestamp(String.valueOf(System.currentTimeMillis()));
-//                        String authMsg_svrNego = svrNegoReq.getSecretId() + "," + svrNegoReq.getSystemId() + "," + svrNegoReq.getServerId() + "," + svrNegoReq.getTimestamp();
-//                        svrNegoReq.setHmac(Base64.toBase64String(SM3Util.hmac(hmacKey, authMsg_svrNego.getBytes(StandardCharsets.UTF_8))));
-//                        Response response_svrNego = okHttpClient.newCall(new Request.Builder()
-//                                .url("https://" + host + "/onlinebizkey/serverNegotiateOnlineBizKey")
-//                                .post(RequestBody.create(GsonUtils.toJson(svrNegoReq), MediaType.parse("application/json; charset=utf-8")))
-//                                .build()).execute();
-//                        if (response_svrNego.isSuccessful()) {
-//                            SvrNegoOLBizKeyResp srvNegoResponse = GsonUtils.fromJson(response_svrNego.body().string(), SvrNegoOLBizKeyResp.class);
-//                            if (srvNegoResponse.getCode() == 0) {
-//                                String secretKey_encrypted_encoded = srvNegoResponse.getData().getSecretKey();
-//                                byte[] secretKey_encrypted = Base64.decode(secretKey_encrypted_encoded);
-//                                byte[] secretKey = SM4Util.decrypt_CBC_Padding(decryptKey, iv, secretKey_encrypted);
-//                                LogUtils.d("服务端在线业务密钥：0x" + ConvertUtils.bytes2HexString(secretKey));
-//
-//                                // step 3: 客户端协商在线业务密钥
-//                                Thread.sleep(1000L); // 客户端协商时间应比服务端协商时间晚，模拟延时操作。
-//                                NegotiateInfo negotiateInfo = qCard.negoOLBizKey(host, deviceId, systemId, secretId, keyAppSvrId, secAuthKey, protectKey);
-//                                if (negotiateInfo != null) {
-//                                    ToastUtils.showLong("在线业务密钥协商成功");
-//                                    negoInfos.add(negotiateInfo);
-//                                } else {
-//                                    ToastUtils.showLong("在线业务密钥协商失败");
-//                                }
-//
-//                                // step 3.1: 获取密钥句柄
-//                                // step 3.2: 导出软密钥
-//                                // step 3.3: 对比客户端软密钥与服务端业务密钥是否一致
-//                                // step 4: 服务端销毁在线业务密钥 (业务结束后调用)
-//                                CleanOLBizKeyReq cleanRequest = new CleanOLBizKeyReq();
-//                                cleanRequest.setSecretId(secretId);
-//                                cleanRequest.setSystemId(systemId);
-//                                cleanRequest.setServerId(keyAppSvrId);
-//                                cleanRequest.setTimestamp(String.valueOf(System.currentTimeMillis()));
-//                                String authMsg_clean = cleanRequest.getSecretId() + "," + cleanRequest.getSystemId() + "," + cleanRequest.getServerId() + "," + cleanRequest.getTimestamp();
-//                                cleanRequest.setHmac(Base64.toBase64String(SM3Util.hmac(hmacKey, authMsg_clean.getBytes(StandardCharsets.UTF_8))));
-//                                Response response_clean = okHttpClient.newCall(new Request.Builder()
-//                                        .url("https://" + host + "/onlinebizkey/cleanNegotiateOnlineBizKey")
-//                                        .post(RequestBody.create(GsonUtils.toJson(cleanRequest), MediaType.parse("application/json; charset=utf-8"))).build()).execute();
-//                                if (response_clean.isSuccessful()) {
-//                                    CleanOLBizKeyResp cleanResponse = GsonUtils.fromJson(response_clean.body().string(), CleanOLBizKeyResp.class);
-//                                    if (cleanResponse.getCode() == 0) {
-//                                        LogUtils.d("清除在线业务密钥成功");
-//                                    } else {
-//                                        ToastUtils.showLong(cleanResponse.getMessage());
-//                                    }
-//                                }
-//                            } else {
-//                                ToastUtils.showLong(srvNegoResponse.getMessage());
-//                            }
-//                        }
-//                    } else {
-//                        ToastUtils.showLong(createResponse.getMessage());
-//                    }
-//                }
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//            runOnUiThread(waitingDialog::dismiss);
-//        }).start();
+        new Thread(() -> {
+            runOnUiThread(waitingDialog::show);
+
+            // step 1: 服务端创建在线业务密钥
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.YEAR, 1);
+            CreateOLBizKeyReq createRequest = new CreateOLBizKeyReq();
+            createRequest.setSecretSize(softKeyLen);
+            createRequest.setValidityDate(TimeUtils.date2String(calendar.getTime()));
+            createRequest.setSystemId(systemId);
+            createRequest.setServerId(keyAppSvrId);
+            createRequest.setTimestamp(System.currentTimeMillis());
+            String authMsg_create = createRequest.getSecretSize() + "," + createRequest.getValidityDate() + "," + createRequest.getSystemId() + "," + createRequest.getServerId() + "," + createRequest.getTimestamp();
+            createRequest.setHmac(Base64.toBase64String(SM3Util.hmac(hmacKey, authMsg_create.getBytes(StandardCharsets.UTF_8))));
+            try {
+                Response response_create = okHttpClient.newCall(new Request.Builder()
+                        .url("https://" + host + "/onlinebizkey/createOnlineBizKey")
+                        .post(RequestBody.create(GsonUtils.toJson(createRequest), MediaType.parse("application/json; charset=utf-8")))
+                        .build()).execute();
+                if (response_create.isSuccessful()) {
+                    CreateOLBizKeyResp createResponse = GsonUtils.fromJson(response_create.body().string(), CreateOLBizKeyResp.class);
+                    if (createResponse.getCode() == 0) {
+                        String secretId = createResponse.getData().getSecretId();
+
+                        // step 2: 服务端协商在线业务密钥
+                        SvrNegoOLBizKeyReq svrNegoReq = new SvrNegoOLBizKeyReq();
+                        svrNegoReq.setSecretId(secretId);
+                        svrNegoReq.setSystemId(systemId);
+                        svrNegoReq.setServerId(keyAppSvrId);
+                        svrNegoReq.setTimestamp(String.valueOf(System.currentTimeMillis()));
+                        String authMsg_svrNego = svrNegoReq.getSecretId() + "," + svrNegoReq.getSystemId() + "," + svrNegoReq.getServerId() + "," + svrNegoReq.getTimestamp();
+                        svrNegoReq.setHmac(Base64.toBase64String(SM3Util.hmac(hmacKey, authMsg_svrNego.getBytes(StandardCharsets.UTF_8))));
+                        Response response_svrNego = okHttpClient.newCall(new Request.Builder()
+                                .url("https://" + host + "/onlinebizkey/serverNegotiateOnlineBizKey")
+                                .post(RequestBody.create(GsonUtils.toJson(svrNegoReq), MediaType.parse("application/json; charset=utf-8")))
+                                .build()).execute();
+                        if (response_svrNego.isSuccessful()) {
+                            SvrNegoOLBizKeyResp srvNegoResponse = GsonUtils.fromJson(response_svrNego.body().string(), SvrNegoOLBizKeyResp.class);
+                            if (srvNegoResponse.getCode() == 0) {
+                                String secretKey_encrypted_encoded = srvNegoResponse.getData().getSecretKey();
+                                byte[] secretKey_encrypted = Base64.decode(secretKey_encrypted_encoded);
+                                byte[] secretKey = SM4Util.decrypt_CBC_Padding(decryptKey, iv, secretKey_encrypted);
+                                LogUtils.d("服务端在线业务密钥：0x" + ConvertUtils.bytes2HexString(secretKey));
+
+                                // step 3: 客户端协商在线业务密钥
+                                Thread.sleep(1000L); // 客户端协商时间应比服务端协商时间晚，模拟延时操作。
+                                NegotiateInfo negotiateInfo = qCard.negoOLBizKey(host, deviceId, systemId, secretId, keyAppSvrId, secAuthKey, protectKey);
+                                if (negotiateInfo != null) {
+                                    ToastUtils.showLong("在线业务密钥协商成功");
+                                    negoInfos.add(negotiateInfo);
+                                } else {
+                                    ToastUtils.showLong("在线业务密钥协商失败");
+                                }
+                                // step 3.1: 获取密钥句柄
+                                // step 3.2: 导出软密钥
+                                // step 3.3: 对比客户端软密钥与服务端业务密钥是否一致
+
+                                // step 4: 服务端销毁在线业务密钥 (业务结束后调用)
+                                CleanOLBizKeyReq cleanRequest = new CleanOLBizKeyReq();
+                                cleanRequest.setSecretId(secretId);
+                                cleanRequest.setSystemId(systemId);
+                                cleanRequest.setServerId(keyAppSvrId);
+                                cleanRequest.setTimestamp(String.valueOf(System.currentTimeMillis()));
+                                String authMsg_clean = cleanRequest.getSecretId() + "," + cleanRequest.getSystemId() + "," + cleanRequest.getServerId() + "," + cleanRequest.getTimestamp();
+                                cleanRequest.setHmac(Base64.toBase64String(SM3Util.hmac(hmacKey, authMsg_clean.getBytes(StandardCharsets.UTF_8))));
+                                Response response_clean = okHttpClient.newCall(new Request.Builder()
+                                        .url("https://" + host + "/onlinebizkey/cleanNegotiateOnlineBizKey")
+                                        .post(RequestBody.create(GsonUtils.toJson(cleanRequest), MediaType.parse("application/json; charset=utf-8"))).build()).execute();
+                                if (response_clean.isSuccessful()) {
+                                    CleanOLBizKeyResp cleanResponse = GsonUtils.fromJson(response_clean.body().string(), CleanOLBizKeyResp.class);
+                                    if (cleanResponse.getCode() == 0) {
+                                        LogUtils.d("清除在线业务密钥成功");
+                                    } else {
+                                        ToastUtils.showLong(cleanResponse.getMessage());
+                                    }
+                                }
+                            } else {
+                                ToastUtils.showLong(srvNegoResponse.getMessage());
+                            }
+                        }
+                    } else {
+                        ToastUtils.showLong(createResponse.getMessage());
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            runOnUiThread(waitingDialog::dismiss);
+        }).start();
     }
 
     long keyHandle;
@@ -496,4 +497,8 @@ public class MainActivity extends AppCompatActivity {
         Log.d(LOG_TAG, "签名公钥：\n" + signPubKeyPem);
     }
 
+    public void verifyAppPIN(View view) {
+        boolean success = qCard.verifyAppPIN(appName, userPIN);
+        ToastUtils.showLong("验证应用PIN: " + (success ? "成功" : "失败"));
+    }
 }
